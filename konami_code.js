c56b8dd5507a1012ea33694c40d7129702728205
Nikki A <nikki.apostolakis@gmail.com>
2019-05-05 17:14:29 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,31 +1,31 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init() 
+{
   // Write your JavaScript code inside the init() function
   let index = 0;
   let body = document.body;
   body.addEventListener('keydown', onKeyDownHandler);
 
-}
-
-function onKeyDownHandler(e)
-{
-  const key = parseInt(e.which);
-
-  if (key === code[index])
+  function onKeyDownHandler(e)
   {
-    index++;
+    const key = parseInt(e.which);
 
-    if (index === code.length)
+    if (key === code[index])
     {
-      alert("Hurray!");
+      index++;
 
+      if (index === code.length)
+      {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    }
+    else
+    {
       index = 0;
     }
-  }
-  else
-  {
-    index = 0;
   }
 }
 init();
